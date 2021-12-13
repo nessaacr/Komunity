9b4f7a0dc41f4d32a96fcbf2d1339a7cdc2854e6
nessaacr <83125081+nessaacr@users.noreply.github.com>
2021-12-13 22:54:53 +0800

    Add files via upload

---
+++ Komunity/scripts/dashboard-sidenav.js
@@ -0,0 +1,7 @@
+function openNav() {
+    document.getElementById("dashboard-sidenav").style.width = "100%";
+  }
+  
+  function closeNav() {
+    document.getElementById("dashboard-sidenav").style.width = "0";
+  }
